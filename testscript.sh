b70b233bbfb4c4ea9f5e2083ae331073cf6a9c1a
electric-blue-green <thomas.bass2001@gmail.com>
2018-03-08 21:12:45 +0000

    asks username

--- testscript.sh
+++ testscript.sh
@@ -8,5 +8,15 @@ echo "# $1" >> README.md
 git add README.md
 touch "$1"."$2"
 git add "$1"."$2"
+echo -n "Please enter your GitHub username: "
+read uname
+git config --global user.email "$uname"
+git commit -m "first commit"
 hub create -d "init commit"
-git commit -m "init commit"
+git remote add origin https://github.com/$uname/$1.git
+git push -u origin master
+
+#git config credential.helper cache
+#hub create -d "init commit"
+#git commit -m "init commit"
+#git push --set-upstream origin master
